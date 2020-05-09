cd923503004710f713fd8f0eea45930b57935a1a
Gabe Stocco <98900+gfs@users.noreply.github.com>
2020-05-08 19:46:51 -0700

    Disable parallel rar processing
    TODO: investigate why it isn't working

--- src/MultiExtractor/Extractor.cs
+++ src/MultiExtractor/Extractor.cs
@@ -231,7 +231,8 @@ namespace Microsoft.CST.OpenSource.MultiExtractor
                         result = ExtractBZip2File(fileEntry);
                         break;
                     case ArchiveFileType.RAR:
-                        result = parallel ? ParallelExtractRarFile(fileEntry) : ExtractRarFile(fileEntry);
+                        //result = parallel ? ParallelExtractRarFile(fileEntry) : ExtractRarFile(fileEntry);
+                        result = ExtractRarFile(fileEntry);
                         break;
                     case ArchiveFileType.P7ZIP:
                         result = parallel ? ParallelExtract7ZipFile(fileEntry) : Extract7ZipFile(fileEntry);

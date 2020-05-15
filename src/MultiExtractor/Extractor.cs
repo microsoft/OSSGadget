a8656e3099d4943c820193b90a4221125e38dd1a
Gabe Stocco <98900+gfs@users.noreply.github.com>
2020-05-15 11:59:23 -0700

    Remove unneeded resource governor check.
    Tiny change to be able to PR and trigger builds.

--- src/MultiExtractor/Extractor.cs
+++ src/MultiExtractor/Extractor.cs
@@ -1167,8 +1167,6 @@ namespace Microsoft.CST.OpenSource.MultiExtractor
                 int batchSize = Math.Min(MAX_BATCH_SIZE, cdFiles.Count);
                 cdFiles.GetRange(0, batchSize).AsParallel().ForAll(cdFile =>
                 {
-                    CheckResourceGovernor(cdFile.Length);
-
                     var fileInfo = cd.GetFileInfo(cdFile);
                     CheckResourceGovernor(fileInfo.Length);
                     var newFileEntry = new FileEntry(fileInfo.Name, fileEntry.FullPath, fileInfo.OpenRead());

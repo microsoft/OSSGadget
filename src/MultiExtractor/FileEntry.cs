ffb165913824512432bbbf21e32a15d5c5d2e4e1
Gabe Stocco <98900+gfs@users.noreply.github.com>
2020-05-17 15:05:47 -0700

    Don't seek if we can't.

--- src/MultiExtractor/FileEntry.cs
+++ src/MultiExtractor/FileEntry.cs
@@ -32,15 +32,16 @@ namespace Microsoft.CST.OpenSource.MultiExtractor
 
             // Back with a temporary filestream, this is optimized to be cached in memory when possible automatically
             Content = new FileStream(Path.GetTempFileName(), FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite, 4096, FileOptions.DeleteOnClose);
-            var initialPosition = inputStream.Position;
+            long? initialPosition = null;
             if (inputStream.CanSeek)
             {
+                initialPosition = inputStream.Position;
                 inputStream.Position = 0;
             }
             inputStream.CopyTo(Content);
             if (inputStream.CanSeek)
             {
-                inputStream.Position = initialPosition;
+                inputStream.Position = initialPosition ?? 0;
             }
             Content.Position = 0;
         }

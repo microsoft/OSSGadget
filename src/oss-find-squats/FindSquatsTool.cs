b96d03d5cecb9fbf4590bed1792620ae774c6571
Gabe Stocco <98900+gfs@users.noreply.github.com>
2021-02-18 20:37:45 -0800

    Fix args

--- src/oss-find-squats/FindSquatsTool.cs
+++ src/oss-find-squats/FindSquatsTool.cs
@@ -9,6 +9,9 @@ using System.Net.Http;
 using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.CST.OpenSource.FindSquats;
+using SarifResult = Microsoft.CodeAnalysis.Sarif.Result;
+using Scriban.Runtime;
+using System.IO;
 
 namespace Microsoft.CST.OpenSource
 {
@@ -39,11 +42,20 @@ namespace Microsoft.CST.OpenSource
                 }
             }
 
-            [Option('o', "output", Required = true, HelpText = "The filename to output.")]
-            public string? OutputFileName { get; set; }
+            [Option('o', "output-file", Required = false, Default = "",
+                HelpText = "send the command output to a file instead of stdout")]
+            public string OutputFile { get; set; } = "";
+
+            [Option('f', "format", Required = false, Default = "text",
+                HelpText = "selct the output format(text|sarifv1|sarifv2)")]
+            public string Format { get; set; } = "text";
+
+            [Option('q', "quiet", Required = false, Default = "text",
+                HelpText = "Suppress console output.")]
+            public bool Quiet { get; set; } = false;
 
             [Value(0, Required = true,
-                HelpText = "PackgeURL(s) specifier to analyze (required)")] // capture all targets to analyze
+                HelpText = "PackgeURL(s) specifier to analyze (required, repeats OK)", Hidden = true)] // capture all targets to analyze
             public IEnumerable<string>? Targets { get; set; }
 
         }
@@ -61,42 +73,40 @@ namespace Microsoft.CST.OpenSource
             await findSquatsTool.ParseOptions<Options>(args).WithParsedAsync(findSquatsTool.RunAsync);
         }
 
-        public async Task RunAsync(Options opts)
+        public async Task RunAsync(Options options)
         {
             var results = new List<(IEnumerable<string> rule, PackageURL purl)>();
+            IOutputBuilder outputBuilder = SelectFormat(options.Format);
 
+            //SarifLog sarifLog = new SarifLog();
+            //sarifLog.Version = SarifVersion.Current;
+            //Run runItem = new Run();
+            //runItem.Tool = new Tool();
 
-            SarifLog sarifLog = new SarifLog();
-            sarifLog.Version = SarifVersion.Current;
-            Run runItem = new Run();
-            runItem.Tool = new Tool();
+            //runItem.Tool.Driver = new ToolComponent();
+            //if (Assembly.GetEntryAssembly() is Assembly entryAssembly)
+            //{
+            //    runItem.Tool.Driver.Name = entryAssembly.GetName().Name;
 
-            runItem.Tool.Driver = new ToolComponent();
-            if (Assembly.GetEntryAssembly() is Assembly entryAssembly)
-            {
-                runItem.Tool.Driver.Name = entryAssembly.GetName().Name;
+            //    runItem.Tool.Driver.FullName = entryAssembly.GetCustomAttribute<AssemblyProductAttribute>()?
+            //                                         .Product;
 
-                runItem.Tool.Driver.FullName = entryAssembly.GetCustomAttribute<AssemblyProductAttribute>()?
-                                                     .Product;
+            //    runItem.Tool.Driver.Version = entryAssembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+            //                                        .InformationalVersion;
+            //}
 
-                runItem.Tool.Driver.Version = entryAssembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
-                                                    .InformationalVersion;
-            }
+            //var descriptors = new List<ReportingDescriptor>();
 
-            var descriptors = new List<ReportingDescriptor>();
+            //foreach (var mutation in gen.Mutations)
+            //{
+            //    ReportingDescriptor sarifRule = new ReportingDescriptor();
+            //    sarifRule.Name = mutation.Method.Name;
+            //    sarifRule.DefaultConfiguration = new ReportingConfiguration() { Enabled = true };
+            //}
 
-            foreach (var mutation in gen.Mutations)
-            {
-                ReportingDescriptor sarifRule = new ReportingDescriptor();
-                sarifRule.Name = mutation.Method.Name;
-                sarifRule.DefaultConfiguration = new ReportingConfiguration() { Enabled = true };
-            }
+            //runItem.Tool.Driver.Rules = descriptors;
 
-            runItem.Tool.Driver.Rules = descriptors;
-
-            var sarifResults = new List<Result>();
-
-            foreach (var target in opts.Targets ?? Array.Empty<string>())
+            foreach (var target in options.Targets ?? Array.Empty<string>())
             {
                 var purl = new PackageURL(target);
                 if (purl.Name is not null && purl.Type is not null)
@@ -108,38 +118,55 @@ namespace Microsoft.CST.OpenSource
 
                         foreach ((var candidate, var rules) in mutationsDict)
                         {
-                            Logger.Info($"Checking {candidate}.");
                             var candidatePurl = new PackageURL(purl.Type, candidate);
                             var versions = await manager.EnumerateVersions(candidatePurl);
 
                             if (versions.Any())
                             {
-                                Logger.Warn($"{candidate} package exists. Potential squat.");
-                                Result res = new Result();
-                                res.Locations = new List<Location>();
-                                Location loc = new Location();
-
-                                loc.LogicalLocation = new LogicalLocation()
+                                if (!options.Quiet)
                                 {
-                                    FullyQualifiedName = purl.ToString()
-                                };
-                                res.Locations.Add(loc);
-                                var rulesString = string.Join(',', rules);
-                                var message = $"Potential Squat candidate {candidate} detected. Generated by {rulesString}";
-                                res.Message = new Message(message, null, rulesString, null, null);
-                                res.Kind = ResultKind.Review;
-                                sarifResults.Add(res);
+                                    Logger.Info($"{candidate} package exists. Potential squat.");
+                                }
+                                if (outputBuilder is SarifOutputBuilder sarob)
+                                {
+                                    SarifResult sarifResult = new SarifResult()
+                                    {
+                                        Message = new Message()
+                                        {
+                                            Text = $"Potential Squat candidate { candidate }.",
+                                            Id = "oss-find-squats"
+                                        },
+                                        Kind = ResultKind.Review,
+                                        Level = FailureLevel.None,
+                                        Locations = SarifOutputBuilder.BuildPurlLocation(purl),
+                                    };
+                                    sarob.AppendOutput(new SarifResult[] { sarifResult });
+                                }
+                                else if (outputBuilder is StringOutputBuilder strob)
+                                {
+                                    var rulesString = string.Join(',', rules);
+                                    strob.AppendOutput(new string[] { $"Potential Squat candidate { candidate } detected. Generated by { rulesString }." });
+                                }
+                                else
+                                {
+                                    var rulesString = string.Join(',', rules);
+                                    if (!options.Quiet)
+                                    {
+                                        Logger.Info($"Potential Squat candidate { candidate } detected. Generated by { rulesString }.");
+                                    }
+                                }
                             }
                         }
                     }
                 }
             }
+            if (string.IsNullOrEmpty(options.OutputFile))
+            {
+                options.OutputFile = $"oss-find-squat.{options.Format}";
+            }
 
-            runItem.Results = sarifResults;
-
-            sarifLog.Runs = new List<Run>();
-            sarifLog.Runs.Add(runItem);
-            sarifLog.Save(opts.OutputFileName);
+            using var fw = new StreamWriter(options.OutputFile);
+            fw.WriteLine(outputBuilder.GetOutput());
         }
     }
 }

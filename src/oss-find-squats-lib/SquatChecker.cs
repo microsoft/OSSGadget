d681981dba10a11cefc639382a44592541e09aba
Gabe Stocco <98900+gfs@users.noreply.github.com>
2021-11-12 12:57:49 -0800

    Update SquatChecker.cs

--- src/oss-find-squats-lib/SquatChecker.cs
+++ src/oss-find-squats-lib/SquatChecker.cs
@@ -8,7 +8,7 @@ namespace Microsoft.CST.OpenSource.FindSquats
 {
     public class SquatChecker
     {
-        Generative gen { get; set; }
+        public Generative Generator { get; set; }
 
         public class Options
         {
@@ -16,22 +16,22 @@ namespace Microsoft.CST.OpenSource.FindSquats
         }
         public SquatChecker()
         {
-            gen = new Generative();
+            Generator = new Generative();
         }
         public static NLog.ILogger Logger { get; set; } = NLog.LogManager.GetCurrentClassLogger();
 
-        public async IAsyncEnumerable<FindSquatResult> CheckSquats(PackageURL purl, Options options)
+        public async IAsyncEnumerable<FindSquatResult> CheckSquats(PackageURL purl, Options? options)
         {
             if (purl.Name is not null && purl.Type is not null)
             {
                 var manager = ProjectManagerFactory.CreateProjectManager(purl, null);
                 if (manager is not null)
                 {
-                    var mutationsDict = gen.Mutate(purl);
+                    var mutationsDict = Generator.Mutate(purl);
 
                     foreach ((var candidate, var rules) in mutationsDict)
                     {
-                        if (options.SleepDelay > 0)
+                        if (options?.SleepDelay > 0)
                         {
                             Thread.Sleep(options.SleepDelay);
                         }

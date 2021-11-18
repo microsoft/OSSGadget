8fc7c31d5cdc36a36329fc8e001fa09c42d35a2d
Gabe Stocco <98900+gfs@users.noreply.github.com>
2021-11-18 11:51:24 -0800

    Update NuGetProjectManager.cs

--- src/Shared/PackageManagers/NuGetProjectManager.cs
+++ src/Shared/PackageManagers/NuGetProjectManager.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
 using System.Xml.Linq;

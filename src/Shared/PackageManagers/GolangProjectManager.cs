bdea2ccebde6f7df6ec6255ee00a76a6275b1e7a
Gabe Stocco <98900+gfs@users.noreply.github.com>
2021-11-18 16:17:56 -0800

    Improve null checks.

--- src/Shared/PackageManagers/GolangProjectManager.cs
+++ src/Shared/PackageManagers/GolangProjectManager.cs
@@ -83,7 +83,7 @@ namespace Microsoft.CST.OpenSource.Shared
         public override async Task<bool> PackageExists(PackageURL purl, bool useCache = true)
         {
             Logger.Trace("PackageExists {0}", purl?.ToString());
-            if (purl is null || purl.Name is null || purl.Type is null || purl.Namespace is null)
+            if (string.IsNullOrEmpty(purl?.Name) || string.IsNullOrEmpty(purl.Namespace))
             {
                 Logger.Trace("Provided PackageURL was null.");
                 return false;

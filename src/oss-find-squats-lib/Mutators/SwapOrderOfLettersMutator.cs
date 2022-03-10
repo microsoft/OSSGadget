f91152465afd9c567fcb90cf28f760e5068980e7
Julian <jupinzer@microsoft.com>
2022-03-10 12:55:30 -0500

    Fix some of the mutators: - No longer generates mutations with a new / at the beginning, so no pkg:npm//lodash (DoubleHit), no pkg:npm//odash (CloseLetters) - No longer generates mutations with the same letters being swapped, mutating pkg:npm/foo and swapping the 'o' generates the same package name, but was considered a mutation.

--- src/oss-find-squats-lib/Mutators/SwapOrderOfLettersMutator.cs
+++ src/oss-find-squats-lib/Mutators/SwapOrderOfLettersMutator.cs
@@ -15,6 +15,12 @@ namespace Microsoft.CST.OpenSource.FindSquats.Mutators
         {
             for (int i = 1; i < arg.Length - 1; i++)
             {
+                // Don't want to swap the same character for the same character, foo -> foo.
+                if (arg[i + 1] == arg[i])
+                {
+                    continue;
+                }
+
                 yield return new Mutation(
                     mutated: string.Concat(arg[..i], arg[i + 1], arg[i], arg.Substring(i + 2, arg.Length - (i + 2))),
                     original: arg,

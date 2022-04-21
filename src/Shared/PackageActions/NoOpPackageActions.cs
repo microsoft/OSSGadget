a9d961556b04725c53fb778bac373b3c0705f319
Julian Pinzer <jupinzer@microsoft.com>
2022-04-21 17:06:03 -0400

    Fixing build failures.

--- src/Shared/PackageActions/NoOpPackageActions.cs
+++ src/Shared/PackageActions/NoOpPackageActions.cs
@@ -11,16 +11,33 @@ using System.Threading.Tasks;
 
 public class NoOpPackageActions : IManagerPackageActions<IManagerPackageVersionMetadata>
 {
-    public Task<string?> DownloadAsync(PackageURL packageUrl, string topLevelDirectory, string targetPath, bool doExtract,
-        bool cached = false, CancellationToken cancellationToken = default) => Task.FromResult<>(null);
+    public Task<string?> DownloadAsync(
+        PackageURL packageUrl,
+        string topLevelDirectory,
+        string targetPath,
+        bool doExtract,
+        bool cached = false,
+        CancellationToken cancellationToken = default) => Task.FromResult<string?>(null);
 
-    public Task<bool> DoesPackageExistAsync(PackageURL packageUrl, bool useCache = true, CancellationToken cancellationToken = default) => Task.FromResult(false);
+    public Task<bool> DoesPackageExistAsync(
+        PackageURL packageUrl,
+        bool useCache = true,
+        CancellationToken cancellationToken = default) => Task.FromResult(false);
 
-    public Task<IEnumerable<string>> GetAllVersionsAsync(PackageURL packageUrl, bool includePrerelease = true, bool useCache = true,
+    public Task<IEnumerable<string>> GetAllVersionsAsync(
+        PackageURL packageUrl,
+        bool includePrerelease = true,
+        bool useCache = true,
         CancellationToken cancellationToken = default) => Task.FromResult(Enumerable.Empty<string>());
 
-    public Task<string?> GetLatestVersionAsync(PackageURL packageUrl, bool includePrerelease = false, bool useCache = true,
-        CancellationToken cancellationToken = default) => Task.FromResult<>(null);
+    public Task<string?> GetLatestVersionAsync(
+        PackageURL packageUrl,
+        bool includePrerelease = false,
+        bool useCache = true,
+        CancellationToken cancellationToken = default) => Task.FromResult<string?>(null);
 
-    public Task<IManagerPackageVersionMetadata?> GetMetadataAsync(PackageURL packageUrl, bool useCache = true, CancellationToken cancellationToken = default) => Task.FromResult<>(null);
+    public Task<IManagerPackageVersionMetadata?> GetMetadataAsync(
+        PackageURL packageUrl,
+        bool useCache = true,
+        CancellationToken cancellationToken = default) => Task.FromResult<IManagerPackageVersionMetadata?>(null);
 }

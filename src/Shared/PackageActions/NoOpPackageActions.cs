1eca83099cffd0c2b069ec918c8b3dc0a4cb593b
Julian Pinzer <jupinzer@microsoft.com>
2022-04-21 16:12:55 -0400

    Change NoOpPackageActions to actually return null or empty instead of throwing NotImplemented exceptions.

--- src/Shared/PackageActions/NoOpPackageActions.cs
+++ src/Shared/PackageActions/NoOpPackageActions.cs
@@ -5,24 +5,22 @@ namespace Microsoft.CST.OpenSource.PackageActions;
 using Contracts;
 using PackageUrl;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
 public class NoOpPackageActions : IManagerPackageActions<IManagerPackageVersionMetadata>
 {
     public Task<string?> DownloadAsync(PackageURL packageUrl, string topLevelDirectory, string targetPath, bool doExtract,
-        bool cached = false, CancellationToken cancellationToken = default) =>
-        throw new System.NotImplementedException();
+        bool cached = false, CancellationToken cancellationToken = default) => Task.FromResult<>(null);
 
-    public Task<bool> DoesPackageExistAsync(PackageURL packageUrl, bool useCache = true, CancellationToken cancellationToken = default) => throw new System.NotImplementedException();
+    public Task<bool> DoesPackageExistAsync(PackageURL packageUrl, bool useCache = true, CancellationToken cancellationToken = default) => Task.FromResult(false);
 
     public Task<IEnumerable<string>> GetAllVersionsAsync(PackageURL packageUrl, bool includePrerelease = true, bool useCache = true,
-        CancellationToken cancellationToken = default) =>
-        throw new System.NotImplementedException();
+        CancellationToken cancellationToken = default) => Task.FromResult(Enumerable.Empty<string>());
 
     public Task<string?> GetLatestVersionAsync(PackageURL packageUrl, bool includePrerelease = false, bool useCache = true,
-        CancellationToken cancellationToken = default) =>
-        throw new System.NotImplementedException();
+        CancellationToken cancellationToken = default) => Task.FromResult<>(null);
 
-    public Task<IManagerPackageVersionMetadata?> GetMetadataAsync(PackageURL packageUrl, bool useCache = true, CancellationToken cancellationToken = default) => throw new System.NotImplementedException();
+    public Task<IManagerPackageVersionMetadata?> GetMetadataAsync(PackageURL packageUrl, bool useCache = true, CancellationToken cancellationToken = default) => Task.FromResult<>(null);
 }

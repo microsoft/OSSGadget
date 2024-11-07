3f111e2bebf941770e05a0c717d0a38307603206
Gabe Stocco <98900+gfs@users.noreply.github.com>
2024-11-07 12:10:24 -0800

    WIP, refactor for OssDownload

---
+++ src/oss-gadget-cli/DownloadTool.cs
@@ -0,0 +1,66 @@
+// Copyright (c) Microsoft Corporation. Licensed under the MIT License.
+
+namespace oss_gadget_cli;
+
+using Microsoft.CST.OpenSource;
+using Microsoft.CST.OpenSource.Helpers;
+using Microsoft.CST.OpenSource.PackageManagers;
+using NLog;
+using PackageUrl;
+
+public class DownloadTool
+{
+    private readonly ProjectManagerFactory _projectManagerFactory;
+    private readonly ILogger _logger;
+
+    public DownloadTool(ProjectManagerFactory projectManagerFactory)
+    {
+        _projectManagerFactory = projectManagerFactory;
+        _logger = CliHelpers.Logger;
+    }
+    
+    public async Task<ErrorCode> RunAsync(DownloadOptions options)
+        {
+            if (options.Targets is IEnumerable<string> targetList && targetList.Any())
+            {
+                foreach (string? target in targetList)
+                {
+                    try
+                    {
+                        // PackageURL requires the @ in a namespace declaration to be escaped
+                        // We find if the namespace contains an @ in the namespace
+                        // And replace it with %40
+                        string escapedNameSpaceTarget = CliHelpers.EscapeAtSymbolInNameSpace(target);
+                        PackageURL? purl = new PackageURL(escapedNameSpaceTarget);
+                        string downloadDirectory = options.DownloadDirectory == "." ? System.IO.Directory.GetCurrentDirectory() : options.DownloadDirectory;
+                        bool useCache = options.UseCache;
+                        PackageDownloader? packageDownloader = new PackageDownloader(purl, _projectManagerFactory, downloadDirectory, useCache);
+
+                        List<string>? downloadResults = await packageDownloader.DownloadPackageLocalCopy(purl, options.DownloadMetadataOnly, options.Extract);
+                        foreach (string? downloadPath in downloadResults)
+                        {
+                            if (string.IsNullOrEmpty(downloadPath))
+                            {
+                                _logger.Error("Unable to download {0}.", purl.ToString());
+                            }
+                            else
+                            {
+                                _logger.Info("Downloaded {0} to {1}", purl.ToString(), downloadPath);
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.Warn(ex, "Error processing {0}: {1}", target, ex.Message);
+                        return ErrorCode.ProcessingException;
+                    }
+                }
+            }
+            else
+            {
+                _logger.Error("No targets were specified for downloading.");
+                return ErrorCode.NoTargets;
+            }
+            return ErrorCode.Ok;
+        }
+}
---
+++ src/oss-gadget-cli/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace oss_gadget_cli;
+
+using CommandLine;
+using Microsoft.CST.OpenSource;
+using System.Reflection;
+
+class OssGadgetCli : OSSGadget
+{
+    static async Task<int> Main(string[] args)
+    {
+        var cli = new OssGadgetCli();
+        await cli.ExecuteAsync(args);
+        return (int)cli._returnCode;
+    }
+
+    private ErrorCode _returnCode = ErrorCode.Ok;
+    
+    //load all types using Reflection
+    private	static Type[] LoadVerbs()
+    {
+        return Assembly.GetExecutingAssembly().GetTypes()
+            .Where(t => t.GetCustomAttribute<VerbAttribute>() != null).ToArray();		 
+    }
+    
+    private async Task Run(object obj)
+    {
+        _returnCode = obj switch
+        {
+            DownloadOptions d => await new DownloadTool(ProjectManagerFactory).RunAsync(d),
+            _ => ErrorCode.Ok
+        };
+    }
+    
+    async Task ExecuteAsync(string[] args)
+    {
+        var verbs = LoadVerbs();
+
+        var res = (await Parser.Default.ParseArguments(args, verbs).WithParsedAsync(Run)).WithNotParsed(HandleErrors);
+    }
+
+    private void HandleErrors(IEnumerable<Error> errs)
+    {
+        _returnCode = errs.Any(x =>
+            x.Tag is not ErrorType.VersionRequestedError
+                and not ErrorType.HelpVerbRequestedError
+                and not ErrorType.HelpRequestedError)
+            ? ErrorCode.ErrorParsingOptions : ErrorCode.Ok;
+    }
+}

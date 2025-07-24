3f891b68daba3b1aaa2f344f1cd11084625c4c86
Dan Fiedler <danfiedler@microsoft.com>
2025-07-24 06:04:15 -0400

    experiment moving test to separate project

---
+++ src/Shared.CLI.IntegrationTests/Helpers/PackageDownloaderTests.cs
@@ -0,0 +1,96 @@
+﻿// Copyright (c) Microsoft Corporation. Licensed under the MIT License.
+
+namespace Microsoft.OSSGadget.Shared.Cli.IntegrationTests.Helpers;
+
+using Microsoft.CST.OpenSource;
+using Microsoft.CST.OpenSource.Helpers;
+using Microsoft.CST.OpenSource.PackageManagers;
+using PackageUrl;
+
+public class PackageDownloaderTheoryData :TheoryData<string,string,int>
+{
+    public PackageDownloaderTheoryData()
+    {
+        Add("pkg:cpan/Data-Rand@0.0.4", "MANIFEST", 1);
+    }
+
+}
+public class PackageDownloaderTests
+{
+    [IntegrationTestTheory]
+    [ClassData(typeof(PackageDownloaderTheoryData))]
+    public async Task When_package_downloaded_then_specified_file_found(string purl, string targetFilename, int expectedDirectoryCount)
+    {
+        string tempDirectoryName = CreateTemporaryDirectory();
+        string errorString = string.Empty;
+
+        try
+        {
+            PackageURL? packageUrl = new(purl);
+            PackageDownloader packageDownloader = packageDownloader = new PackageDownloader(packageUrl, new ProjectManagerFactory(), tempDirectoryName, false);
+            List<string> directories = await packageDownloader.DownloadPackageLocalCopy(packageUrl, false, true);
+            if(directories.Count == 0)
+            {
+                errorString = "Package download resulted in 0 directories. ";
+            }
+
+            bool targetFileWasDownloaded = Directory.EnumerateFiles(tempDirectoryName, targetFilename, SearchOption.AllDirectories).Any();
+            if (!targetFileWasDownloaded)
+            {
+                errorString += "Target file was not downloaded. ";
+            }
+
+            int topLevelDirectoryCount = Directory.GetDirectories(tempDirectoryName).Length;
+            if (expectedDirectoryCount != topLevelDirectoryCount)
+            {
+                errorString += $"Directory count {topLevelDirectoryCount} does not match expected {expectedDirectoryCount}";
+            }
+
+            // one delete is enough, since its only a single cached copy
+            DeleteTempDirs(packageDownloader, tempDirectoryName);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("Error", ex);
+        }
+
+        if(!string.IsNullOrEmpty(errorString))
+        {
+            throw new Exception(errorString);
+        }
+    }
+
+    private static string CreateTemporaryDirectory()
+    {
+        string? tempDirectoryName = null;
+        while (tempDirectoryName == null || Directory.Exists(tempDirectoryName) || File.Exists(tempDirectoryName))
+        {
+            tempDirectoryName = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+        }
+        Directory.CreateDirectory(tempDirectoryName);
+        return tempDirectoryName;
+    }
+
+    private static void DeleteTempDirs(PackageDownloader? packageDownloader, string tempDirectoryName)
+    {
+        try
+        {
+            packageDownloader?.ClearPackageLocalCopyIfNoCaching();
+        }
+        catch (Exception)
+        {
+            foreach (string? filename in Directory.EnumerateFileSystemEntries(tempDirectoryName, "*", SearchOption.AllDirectories))
+            {
+                FileInfo _ = new(filename)
+                {
+                    Attributes = FileAttributes.Normal
+                };
+            }
+            packageDownloader?.ClearPackageLocalCopyIfNoCaching();
+        }
+        finally
+        {
+            FileSystemHelper.RetryDeleteDirectory(tempDirectoryName);
+        }
+    }
+}
---
+++ src/Shared.CLI.IntegrationTests/IntegrationTestFactAttribute.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Microsoft Corporation. Licensed under the MIT License.
+
+namespace Microsoft.OSSGadget.Shared.Cli.IntegrationTests;
+
+public static class IntegrationTestEnvironmentHelper
+{
+    public static bool IsRunningInCI()
+    {
+        return Environment.GetEnvironmentVariable("TF_BUILD")?.ToLowerInvariant() == "true";
+    }
+}
+
+public class IntegrationTestFactAttribute : FactAttribute
+{
+    public IntegrationTestFactAttribute()
+    {
+        if(IntegrationTestEnvironmentHelper.IsRunningInCI())
+        {
+            Skip = "Integration test skipped in ADO CI";
+        }
+    }
+}
+
+public class IntegrationTestTheoryAttribute : TheoryAttribute
+{
+    public IntegrationTestTheoryAttribute()
+    {
+        if (IntegrationTestEnvironmentHelper.IsRunningInCI())
+        {
+            Skip = "Integration test skipped in ADO CI";
+        }
+    }
+}
